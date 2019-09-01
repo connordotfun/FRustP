3bd627a3909e62157452edb0553a4ba323fefc77
Ethan Hunter <fc.spaceman@gmail.com>
2019-08-31 19:19:20 -0600

    oh god

--- src/graph/signal.rs
+++ src/graph/signal.rs
@@ -1,5 +1,6 @@
+use graph;
 
-struct Signal<T: std::marker::Sized>{
+struct Signal<T>{
     inner: T
 }
 
@@ -9,16 +10,16 @@ struct Signal<T: std::marker::Sized>{
 //         f: Fn(T) -> B )
 //         -> Signal<B>;
 // }
-
 // todo: sort out a more complicated generic using where clause that disallows Signal<Signal> types.
 impl<T> Signal<T>{ 
 
-    fn fmap<B:std::marker::Sized> (signal : Signal<T>, f : Fn(T) -> B ) 
-        -> Signal<B>{
-            
-        }
+    fn fmap<B, F: Fn(T) -> B> (signal : &Signal<T>, f: F) -> &Signal<B> {
+        
+    }
 
 }
+
+impl<u32> Singal<T>
 
 // pub trait Functor<A> {
 //     trait<T> inner_object;
@@ -28,4 +29,3 @@ impl<T> Signal<T>{
 //         f : Fn(T) -> G )
 //         -> impl inner_object<G>;
 // }
-
